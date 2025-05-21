112ea7c11fc1117ce7d80ef623789be59868df26
timsalzer8 <t.salzer@yahoo.de>
2025-05-21 17:56:46 +0200

    max 3 turns aded

--- src/Ratespiel.java
+++ src/Ratespiel.java
@@ -5,41 +5,59 @@ public class Ratespiel {
     static int min = 1;
     static int max = 10;
     static int myNumber = ThreadLocalRandom.current().nextInt(min, max + 1);
+    static int tries;
+    static boolean guessedCorrectly;
 
     public static void main(String[] args) {
-        nextRound();
+        System.out.println("Willkommen beim Ratespiel!");
+        System.out.println("Du hast 3 Versuche und glaub ja nicht es gibt irgendwelche Tipps!");
+        System.out.println("");
+        startGame();
+        Scanner scanner = new Scanner(System.in);
+
     }
 
-    public static void nextRound() {
-        Scanner scanner = new Scanner(System.in);
+    public static void  startGame(Scanner scanner) {
+        tries = 0;
+        while (tries < 3 && !guessedCorrectly) {
+            nextRound(scanner);}
+        scanner.close();
+        System.out.println("Spiel beendet.");
+        System.out.println("Möchtest du noch einmal spielen? (j/n)");
+    }
+
+    public static void nextRound(Scanner scanner) {
+        tries = tries +1;
+        if (tries <= 3)
         System.out.println("Gib eine Zahl zwischen 1 und 10 ein: ");
+        if (tries <= 3)
         System.out.println("Pssst, hier ein kleiner Tipp: " + myNumber);
         int number = scanner.nextInt();
+        scanner.nextLine();
         guess(number);
     }
 
     public static void guess(int number) {
         if (number == myNumber) {
             System.out.println("Richtig geraten!");
-            System.out.println();
+            guessedCorrectly = true;
+            System.out.println("Du hast " + tries + " Versuch/e gebraucht.");
         } else {
             if (number < myNumber) {
                 System.out.println("Zu niedrig!");
+                if (tries < 3)
                 System.out.println("Versuche es noch einmal!");
                 System.out.println("");
             }
             if (number > myNumber) {
                 System.out.println("Zu hoch!");
+                if (tries < 3)
                 System.out.println("Versuche es noch einmal!");
                 System.out.println("");
             }
-            nextRound();
+            if (tries >= 3) {
+                System.out.println("Verloren! Du musst die Augen aufmachen um den Tipp " + myNumber + " zu sehen.");
+            }
         }
     }
-    /*
-    public static void tries(int number) {
-        if guess();
-    }
-     */
-
 }
